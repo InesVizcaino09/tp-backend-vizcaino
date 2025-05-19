fc5df6db840040bdbcf743b28c817dff5c0b08eb
InesVizcaino <inesvizcainoestudio@gmail.com>
2025-05-19 18:54:49 -0300

    segundo commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log("Probando...")
+import { Schema, model, connect } from "mongoose";
+
+process.loadEnvFile()
+const URI_DB = process.env.URI_DB || ""
+
+const connectMongoDb = async () => {
+    try {
+        await connect (URI_DB)
+        console.log("Conectado a MongoDB")
+    } catch (errorr) {
+        console.error("error al conectarse a MongoDB")
+    }
+}
+
+const perfSchema = new Schema({
+    title: {type: String , required:true , unique:true} ,
+    brand: {type: String , required:true , unique:true} ,
+    topnote: {type: String , required:true , unique:true} ,
+})
+const Perfume = model("Perfume", perfSchema)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connectMongoDb()
